624b1b10802552c7394bd8c90c792c3a6e0c5d62
Sakusammakko <lucas.harjunpaa@gmail.com>
2021-02-16 19:34:33 +0200

    Heroku

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const app = express()
-
+const app = express() 
 app.use(express.json())
+
+const cors = require('cors')
+app.use(cors())
 
 morgan.token('body', (req, res) => 
   req.method === 'POST' 
